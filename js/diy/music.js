7385f813b5ed9be943c6781a1a326c231573f47b
ChengJIN <jin16118@gmail.com>
2024-07-08 02:54:50 +0800

    Site updated: 2024-07-08 02:54:49

--- js/diy/music.js
+++ js/diy/music.js
@@ -15,21 +15,21 @@ const ap = new APlayer({
             artist: 'Chris James',
             url: 'https://github.com/MINELLO6/mymusic/blob/main/Not Angry.mp3?raw=true',
             cover: 'https://p2.music.126.net/8u9G6-hnZZV3HFZxU7RqHw==/109951165528742706.jpg?param=34y34',
-            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/Not+Angry-Chris+James.lrc'
+            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/Not+Angry-Chris+James.lrc?raw=true'
         },
         {
             name: 'hill that i\'ll die on',
             artist: 'Jonah Kagen',
             url: 'https://github.com/MINELLO6/mymusic/blob/main/hill that i will die one.mp3?raw=true',
             cover: 'https://y.qq.com/music/photo_new/T002R300x300M000002WfJcX16BEzQ_1.jpg?max_age=2592000',
-            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/hill+that+ill+die+on-Jonah+Kagen.lrc'
+            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/hill+that+ill+die+on-Jonah+Kagen.lrc?raw=true'
         },
         {
             name: '普通朋友',
             artist: '陶喆',
             url: 'https://github.com/MINELLO6/mymusic/blob/main/普通朋友.mp3?raw=true',
             cover: 'https://y.qq.com/music/photo_new/T002R300x300M000002PjDkE14OUkV_1.jpg?max_age=2592000',
-            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/普通朋友-陶喆.lrc'
+            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/普通朋友-陶喆.lrc?raw=true'
         }
     ]
 });
