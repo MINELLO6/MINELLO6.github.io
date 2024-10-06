580ebddf3b0f4a1e3b20340fdec7696f9d15bf09
ChengJIN <jin16118@gmail.com>
2024-10-06 15:01:27 +0100

    Site updated: 2024-10-06 15:01:25

--- js/diy/music.js
+++ js/diy/music.js
@@ -15,7 +15,7 @@ const ap = new APlayer({
             artist: 'Chris James',
             url: 'https://github.com/MINELLO6/mymusic/blob/main/Not Angry.mp3?raw=true',
             cover: 'https://p2.music.126.net/8u9G6-hnZZV3HFZxU7RqHw==/109951165528742706.jpg?param=34y34',
-            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/Not+Angry-Chris+James.lrc?raw=true'
+            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/2.lrc?raw=true'
         },
         {
             name: 'hill that i\'ll die on',
@@ -29,7 +29,7 @@ const ap = new APlayer({
             artist: '陶喆',
             url: 'https://github.com/MINELLO6/mymusic/blob/main/普通朋友.mp3?raw=true',
             cover: 'https://y.qq.com/music/photo_new/T002R300x300M000002PjDkE14OUkV_1.jpg?max_age=2592000',
-            lrc: 'https://github.com/MINELLO6/mymusic/blob/main/普通朋友-陶喆.lrc?raw=true'
+            lrc: '[00:00.00] 我们一起学猫叫\n[99:99.99] 一起喵喵喵喵喵'
         }
     ]
 });
